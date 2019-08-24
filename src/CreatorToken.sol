1b46a836b8fd451d1adf4bf9cb1b8b1ae8175d3b
Jonathan Brown <jbrown@mix-blockchain.org>
2019-08-24 11:34:19 +0800

    Implement initial balance.

--- src/CreatorToken.sol
+++ src/CreatorToken.sol
@@ -7,20 +7,22 @@ contract CreatorToken is MixTokenBase {
 
     uint public tokenStart;
     address public tokenOwner;
-    uint public tokenPayout;
+    uint public tokenInitialBalance;
+    uint public tokenDailyPayout;
 
-    constructor(string memory symbol, string memory name, uint decimals, uint payout, address owner, MixTokenRegistry tokenRegistry, bytes32 itemId) public
+    constructor(string memory symbol, string memory name, uint decimals, uint initialBalance, uint dailyPayout, address owner, MixTokenRegistry tokenRegistry, bytes32 itemId) public
         MixTokenBase(symbol, name, decimals, tokenRegistry, itemId)
     {
         accountState[owner].inUse = true;
         accountList.push(owner);
         tokenStart = block.timestamp;
         tokenOwner = owner;
-        tokenPayout = payout;
+        tokenInitialBalance = initialBalance;
+        tokenDailyPayout = dailyPayout;
     }
 
     function totalSupply() public view returns (uint) {
-        return ((block.timestamp - tokenStart) * tokenPayout) / 1 days;
+        return tokenInitialBalance + ((block.timestamp - tokenStart) * tokenDailyPayout) / 1 days;
     }
 
     function balanceOf(address account) public view returns (uint balance) {
