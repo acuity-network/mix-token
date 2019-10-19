aa96a68af91a237a9427d3adede2369ff5ccaf81
Jonathan Brown <jbrown@mix-blockchain.org>
2019-10-19 17:13:03 +0700

    Update tests.

--- src/MixTokenBurn.t.sol
+++ src/MixTokenBurn.t.sol
@@ -11,12 +11,12 @@ contract Token is MixTokenInterface, MixTokenBase {
         MixTokenBase(symbol, name, tokenRegistry, itemId)
     {
         accountState[msg.sender].inUse = true;
-        accountState[msg.sender].balance = 10;
+        accountState[msg.sender].balance = 100;
         accountList.push(msg.sender);
     }
 
     function totalSupply() external view returns (uint) {
-        return 10;
+        return 100;
     }
 
 }
@@ -35,12 +35,12 @@ contract AccountProxy {
         mixTokenBase.authorize(account);
     }
 
-    function burnToken(MixTokenInterface token, uint amount, address prev, address next) external {
-        mixTokenBurn.burnToken(token, amount, prev, next);
-    }
-
     function getBurnTokenPrevNext(MixTokenInterface token, uint amount) external view returns (address prev, address next) {
         (prev, next) = mixTokenBurn.getBurnTokenPrevNext(token, amount);
+    }
+
+    function burnToken(MixTokenInterface token, uint amount, address prev, address next) external {
+        mixTokenBurn.burnToken(token, amount, prev, next);
     }
 
 }
@@ -55,6 +55,12 @@ contract MixTokenBurnTest is DSTest {
     MixItemDagOneParent mixTokenItems;
     MixTokenBurn mixTokenBurn;
 
+    AccountProxy account0;
+    AccountProxy account1;
+    AccountProxy account2;
+    AccountProxy account3;
+    AccountProxy account4;
+
     function setUp() public {
         mixItemStoreRegistry = new MixItemStoreRegistry();
         mixItemStore = new MixItemStoreIpfsSha256(mixItemStoreRegistry);
@@ -64,84 +70,73 @@ contract MixTokenBurnTest is DSTest {
         mixTokenItems = new MixItemDagOneParent(mixItemStoreRegistry);
         mixTokenBurn = new MixTokenBurn(mixTokenRegistry, mixTokenItems);
         token.authorize(address(mixTokenBurn));
+
+        account0 = new AccountProxy(token, mixTokenBurn);
+        account1 = new AccountProxy(token, mixTokenBurn);
+        account2 = new AccountProxy(token, mixTokenBurn);
+        account3 = new AccountProxy(token, mixTokenBurn);
+        account4 = new AccountProxy(token, mixTokenBurn);
+
+        token.transfer(address(account0), 2);
+        token.transfer(address(account1), 2);
+        token.transfer(address(account2), 2);
+        token.transfer(address(account3), 2);
+        token.transfer(address(account4), 2);
+
+        account0.authorize(address(mixTokenBurn));
+        account1.authorize(address(mixTokenBurn));
+        account2.authorize(address(mixTokenBurn));
+        account3.authorize(address(mixTokenBurn));
+        account4.authorize(address(mixTokenBurn));
     }
 
     function testControlBurnTokenZero() public {
-        (address prev, address next) = mixTokenBurn.getBurnTokenPrevNext(token, 1);
-        mixTokenBurn.burnToken(token, 1, prev, next);
+        (address prev, address next) = account0.getBurnTokenPrevNext(token, 1);
+        account0.burnToken(token, 1, prev, next);
     }
 
     function testFailBurnTokenZero() public {
-        (address prev, address next) = mixTokenBurn.getBurnTokenPrevNext(token, 0);
-        mixTokenBurn.burnToken(token, 0, prev, next);
+        (address prev, address next) = account0.getBurnTokenPrevNext(token, 0);
+        account0.burnToken(token, 0, prev, next);
     }
 
     function testControlBurnTokenNotEnough() public {
-        (address prev, address next) = mixTokenBurn.getBurnTokenPrevNext(token, 10);
-        mixTokenBurn.burnToken(token, 10, prev, next);
+        (address prev, address next) = account0.getBurnTokenPrevNext(token, 2);
+        account0.burnToken(token, 2, prev, next);
     }
 
     function testFailBurnTokenNotEnough() public {
-        (address prev, address next) = mixTokenBurn.getBurnTokenPrevNext(token, 11);
-        mixTokenBurn.burnToken(token, 11, prev, next);
+        (address prev, address next) = account0.getBurnTokenPrevNext(token, 3);
+        account0.burnToken(token, 3, prev, next);
     }
 
     function testBurnToken() public {
-        assertEq(token.balanceOf(address(this)), 10);
-        assertEq(mixTokenBurn.getAccountTokenBurned(address(this), token), 0);
-        (address prev, address next) = mixTokenBurn.getBurnTokenPrevNext(token, 1);
-        mixTokenBurn.burnToken(token, 1, prev, next);
-        assertEq(token.balanceOf(address(this)), 9);
-        assertEq(mixTokenBurn.getAccountTokenBurned(address(this), token), 1);
+        assertEq(token.balanceOf(address(account0)), 2);
+        assertEq(mixTokenBurn.getAccountTokensBurnedCount(address(account0)), 0);
+        assertEq(mixTokenBurn.getAccountTokenBurned(address(account0), token), 0);
+        assertEq(mixTokenBurn.getAccountTokensBurnedCount(address(account1)), 0);
+        assertEq(mixTokenBurn.getAccountTokenBurned(address(account1), token), 0);
 
-        AccountProxy accountProxy = new AccountProxy(token, mixTokenBurn);
+        (address prev, address next) = account0.getBurnTokenPrevNext(token, 1);
+        account0.burnToken(token, 1, prev, next);
+        assertEq(token.balanceOf(address(account0)), 1);
+        assertEq(token.balanceOf(address(mixTokenBurn)), 1);
+        assertEq(mixTokenBurn.getAccountTokensBurnedCount(address(account0)), 1);
+        assertEq(mixTokenBurn.getAccountTokenBurned(address(account0), token), 1);
+        assertEq(mixTokenBurn.getAccountTokensBurnedCount(address(account1)), 0);
+        assertEq(mixTokenBurn.getAccountTokenBurned(address(account1), token), 0);
+        (address[] memory tokens, uint[] memory amounts) = mixTokenBurn.getAccountTokensBurned(address(account0), 0, 0);
+        assertEq(tokens.length, 1);
+        assertEq(amounts.length, 1);
 
-        token.transfer(address(accountProxy), 2);
-
-        accountProxy.authorize(address(mixTokenBurn));
-        (prev, next) = accountProxy.getBurnTokenPrevNext(token, 2);
-
-        emit log_named_address("prev", prev);
-        emit log_named_address("next", next);
-
-        accountProxy.burnToken(token, 2, prev, next);
-        assertEq(token.balanceOf(address(accountProxy)), 0);
-        assertEq(mixTokenBurn.getAccountTokenBurned(address(this), token), 1);
-        assertEq(mixTokenBurn.getAccountTokenBurned(address(accountProxy), token), 2);
-/*
-        (prev, next) = mixTokenBurn.getBurnTokensPrevNext(token, 2);
-        mixTokenBurn.burnTokens(token, 2, prev, next);
-        assertEq(token.balanceOf(address(this)), 2);
-        assertEq(mixTokenBurn.getAccountTokenBurned(address(this), token), 7);
-
-        (prev, next) = mixTokenBurn.getBurnTokensPrevNext(token, 2);
-        mixTokenBurn.burnTokens(token, 2, prev, next);
-        assertEq(token.balanceOf(address(this)), 0);
-        assertEq(mixTokenBurn.getAccountTokenBurned(address(this), token), 9);
-*/
+        (prev, next) = account1.getBurnTokenPrevNext(token, 2);
+        account1.burnToken(token, 2, prev, next);
+        assertEq(token.balanceOf(address(account1)), 0);
+        assertEq(token.balanceOf(address(mixTokenBurn)), 3);
+        assertEq(mixTokenBurn.getAccountTokensBurnedCount(address(account0)), 1);
+        assertEq(mixTokenBurn.getAccountTokenBurned(address(account0), token), 1);
+        assertEq(mixTokenBurn.getAccountTokensBurnedCount(address(account1)), 1);
+        assertEq(mixTokenBurn.getAccountTokenBurned(address(account1), token), 2);
     }
-/*
-    function testGetTokensBurnedMultiple() public {
-        bytes32 itemId = mixItemStore.create(hex"0201", hex"1234");
-        MixTokenInterface[] memory tokens = new MixTokenInterface[](4);
-        tokens[0] = new Token('a', 'A', mixTokenRegistry, itemId);
-        tokens[1] = new Token('a', 'A', mixTokenRegistry, itemId);
-        tokens[2] = new Token('a', 'A', mixTokenRegistry, itemId);
-        tokens[3] = new Token('a', 'A', mixTokenRegistry, itemId);
-        tokens[0].authorize(address(mixTokenBurn));
-        tokens[1].authorize(address(mixTokenBurn));
-        tokens[2].authorize(address(mixTokenBurn));
-        tokens[3].authorize(address(mixTokenBurn));
-        mixTokenBurn.burnTokens(tokens[0], 5);
-        mixTokenBurn.burnTokens(tokens[1], 4);
-        mixTokenBurn.burnTokens(tokens[2], 3);
-        mixTokenBurn.burnTokens(tokens[3], 2);
-        uint[] memory burned = mixTokenBurn.getTokensBurnedMultiple(address(this), tokens);
-        assertEq(burned.length, 4);
-        assertEq(burned[0], 5);
-        assertEq(burned[1], 4);
-        assertEq(burned[2], 3);
-        assertEq(burned[3], 2);
-    }
-*/
+
 }
