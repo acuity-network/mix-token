d2a113d79acaf079ef1ada8ad8a772def456d931
Jonathan Brown <jbrown@mix-blockchain.org>
2019-11-02 17:30:14 +0700

    token => mixCreatorToken

--- src/MixCreatorToken.t.sol
+++ src/MixCreatorToken.t.sol
@@ -11,7 +11,7 @@ contract MixCreatorTokenTest is DSTest {
     MixTokenItemRegistry mixTokenRegistry;
     MixItemStoreRegistry mixItemStoreRegistry;
     MixItemStoreIpfsSha256 mixItemStore;
-    MixCreatorToken token;
+    MixCreatorToken mixCreatorToken;
     MockAccount mockAccount;
 
     function setUp() public {
@@ -19,119 +19,119 @@ contract MixCreatorTokenTest is DSTest {
         mixItemStore = new MixItemStoreIpfsSha256(mixItemStoreRegistry);
         bytes32 itemId = mixItemStore.create(hex"02", hex"1234");
         mixTokenRegistry = new MixTokenItemRegistry(mixItemStoreRegistry);
-        token = new MixCreatorToken('a', 'A', address(this), 10, 1);
-        mixTokenRegistry.register(token, itemId);
-        mockAccount = new MockAccount(token);
+        mixCreatorToken = new MixCreatorToken('a', 'A', address(this), 10, 1);
+        mixTokenRegistry.register(mixCreatorToken, itemId);
+        mockAccount = new MockAccount(mixCreatorToken);
     }
 
     function testConstants() public {
-        assertEq0(bytes(token.symbol()), bytes('a'));
-        assertEq0(bytes(token.name()), bytes('A'));
-        assertEq(token.decimals(), 18);
-        assertEq(token.totalSupply(), 10);
-        assertEq(token.start(), block.timestamp);
-        assertEq(token.owner(), address(this));
-        assertEq(token.initialBalance(), 10);
-        assertEq(token.dailyPayout(), 1);
+        assertEq0(bytes(mixCreatorToken.symbol()), bytes('a'));
+        assertEq0(bytes(mixCreatorToken.name()), bytes('A'));
+        assertEq(mixCreatorToken.decimals(), 18);
+        assertEq(mixCreatorToken.totalSupply(), 10);
+        assertEq(mixCreatorToken.start(), block.timestamp);
+        assertEq(mixCreatorToken.owner(), address(this));
+        assertEq(mixCreatorToken.initialBalance(), 10);
+        assertEq(mixCreatorToken.dailyPayout(), 1);
     }
 
     function testControlTransferInsufficientBalance() public {
-        token.transfer(address(0x1234), 1);
+        mixCreatorToken.transfer(address(0x1234), 1);
     }
 
     function testFailTransferInsufficientBalance() public {
-        token.transfer(address(0x1234), 10);
-        token.transfer(address(0x1234), 1);
+        mixCreatorToken.transfer(address(0x1234), 10);
+        mixCreatorToken.transfer(address(0x1234), 1);
     }
 
     function testTransfer() public {
-        assertEq(token.balanceOf(address(this)), 10);
-        assertEq(token.balanceOf(address(0x1234)), 0);
-        assertEq(token.balanceOf(address(0x2345)), 0);
-        assertTrue(token.transfer(address(0x1234), 5));
-        assertEq(token.balanceOf(address(this)), 5);
-        assertEq(token.balanceOf(address(0x1234)), 5);
-        assertEq(token.balanceOf(address(0x2345)), 0);
-        assertTrue(token.transfer(address(0x1234), 2));
-        assertEq(token.balanceOf(address(this)), 3);
-        assertEq(token.balanceOf(address(0x1234)), 7);
-        assertEq(token.balanceOf(address(0x2345)), 0);
-        assertTrue(token.transfer(address(0x2345), 1));
-        assertEq(token.balanceOf(address(this)), 2);
-        assertEq(token.balanceOf(address(0x1234)), 7);
-        assertEq(token.balanceOf(address(0x2345)), 1);
-        assertTrue(token.transfer(address(0x2345), 2));
-        assertEq(token.balanceOf(address(this)), 0);
-        assertEq(token.balanceOf(address(0x1234)), 7);
-        assertEq(token.balanceOf(address(0x2345)), 3);
+        assertEq(mixCreatorToken.balanceOf(address(this)), 10);
+        assertEq(mixCreatorToken.balanceOf(address(0x1234)), 0);
+        assertEq(mixCreatorToken.balanceOf(address(0x2345)), 0);
+        assertTrue(mixCreatorToken.transfer(address(0x1234), 5));
+        assertEq(mixCreatorToken.balanceOf(address(this)), 5);
+        assertEq(mixCreatorToken.balanceOf(address(0x1234)), 5);
+        assertEq(mixCreatorToken.balanceOf(address(0x2345)), 0);
+        assertTrue(mixCreatorToken.transfer(address(0x1234), 2));
+        assertEq(mixCreatorToken.balanceOf(address(this)), 3);
+        assertEq(mixCreatorToken.balanceOf(address(0x1234)), 7);
+        assertEq(mixCreatorToken.balanceOf(address(0x2345)), 0);
+        assertTrue(mixCreatorToken.transfer(address(0x2345), 1));
+        assertEq(mixCreatorToken.balanceOf(address(this)), 2);
+        assertEq(mixCreatorToken.balanceOf(address(0x1234)), 7);
+        assertEq(mixCreatorToken.balanceOf(address(0x2345)), 1);
+        assertTrue(mixCreatorToken.transfer(address(0x2345), 2));
+        assertEq(mixCreatorToken.balanceOf(address(this)), 0);
+        assertEq(mixCreatorToken.balanceOf(address(0x1234)), 7);
+        assertEq(mixCreatorToken.balanceOf(address(0x2345)), 3);
     }
 
     function testControlTransferFromNotAuthorized() public {
         mockAccount.authorize(address(this));
-        token.transfer(address(mockAccount), 5);
-        token.transferFrom(address(mockAccount), address(this), 5);
+        mixCreatorToken.transfer(address(mockAccount), 5);
+        mixCreatorToken.transferFrom(address(mockAccount), address(this), 5);
     }
 
     function testFailTransferFromNotAuthorized() public {
-        token.transfer(address(mockAccount), 5);
-        token.transferFrom(address(mockAccount), address(this), 5);
+        mixCreatorToken.transfer(address(mockAccount), 5);
+        mixCreatorToken.transferFrom(address(mockAccount), address(this), 5);
     }
 
     function testControlTransferFromInsufficientBalance() public {
         mockAccount.authorize(address(this));
-        token.transfer(address(mockAccount), 5);
-        token.transferFrom(address(mockAccount), address(this), 5);
+        mixCreatorToken.transfer(address(mockAccount), 5);
+        mixCreatorToken.transferFrom(address(mockAccount), address(this), 5);
     }
 
     function testFailTransferFromInsufficientBalance() public {
         mockAccount.authorize(address(this));
-        token.transferFrom(address(mockAccount), address(this), 5);
+        mixCreatorToken.transferFrom(address(mockAccount), address(this), 5);
     }
 
     function testTransferFrom() public {
-        assertEq(token.balanceOf(address(this)), 10);
-        assertEq(token.balanceOf(address(mockAccount)), 0);
-        assertEq(token.balanceOf(address(0x1234)), 0);
+        assertEq(mixCreatorToken.balanceOf(address(this)), 10);
+        assertEq(mixCreatorToken.balanceOf(address(mockAccount)), 0);
+        assertEq(mixCreatorToken.balanceOf(address(0x1234)), 0);
         mockAccount.authorize(address(this));
-        assertTrue(token.transfer(address(mockAccount), 10));
-        assertEq(token.balanceOf(address(mockAccount)), 10);
-        assertEq(token.balanceOf(address(this)), 0);
-        assertEq(token.balanceOf(address(0x1234)), 0);
-        assertTrue(token.transferFrom(address(mockAccount), address(this), 3));
-        assertEq(token.balanceOf(address(mockAccount)), 7);
-        assertEq(token.balanceOf(address(this)), 3);
-        assertEq(token.balanceOf(address(0x1234)), 0);
-        assertTrue(token.transferFrom(address(mockAccount), address(0x1234), 5));
-        assertEq(token.balanceOf(address(mockAccount)), 2);
-        assertEq(token.balanceOf(address(this)), 3);
-        assertEq(token.balanceOf(address(0x1234)), 5);
-        assertTrue(token.transferFrom(address(mockAccount), address(this), 2));
-        assertEq(token.balanceOf(address(mockAccount)), 0);
-        assertEq(token.balanceOf(address(this)), 5);
-        assertEq(token.balanceOf(address(0x1234)), 5);
+        assertTrue(mixCreatorToken.transfer(address(mockAccount), 10));
+        assertEq(mixCreatorToken.balanceOf(address(mockAccount)), 10);
+        assertEq(mixCreatorToken.balanceOf(address(this)), 0);
+        assertEq(mixCreatorToken.balanceOf(address(0x1234)), 0);
+        assertTrue(mixCreatorToken.transferFrom(address(mockAccount), address(this), 3));
+        assertEq(mixCreatorToken.balanceOf(address(mockAccount)), 7);
+        assertEq(mixCreatorToken.balanceOf(address(this)), 3);
+        assertEq(mixCreatorToken.balanceOf(address(0x1234)), 0);
+        assertTrue(mixCreatorToken.transferFrom(address(mockAccount), address(0x1234), 5));
+        assertEq(mixCreatorToken.balanceOf(address(mockAccount)), 2);
+        assertEq(mixCreatorToken.balanceOf(address(this)), 3);
+        assertEq(mixCreatorToken.balanceOf(address(0x1234)), 5);
+        assertTrue(mixCreatorToken.transferFrom(address(mockAccount), address(this), 2));
+        assertEq(mixCreatorToken.balanceOf(address(mockAccount)), 0);
+        assertEq(mixCreatorToken.balanceOf(address(this)), 5);
+        assertEq(mixCreatorToken.balanceOf(address(0x1234)), 5);
     }
 
     function testAccountList() public {
         address[] memory accounts;
         uint[] memory balances;
 
-        assertEq(token.getAccountCount(), 1);
-        accounts = token.getAccounts();
+        assertEq(mixCreatorToken.getAccountCount(), 1);
+        accounts = mixCreatorToken.getAccounts();
         assertEq(accounts.length, 1);
         assertEq(accounts[0], address(this));
-        (accounts, balances) = token.getAccountBalances();
+        (accounts, balances) = mixCreatorToken.getAccountBalances();
         assertEq(accounts.length, 1);
         assertEq(accounts[0], address(this));
         assertEq(balances.length, 1);
         assertEq(balances[0], 10);
 
-        assertTrue(token.transfer(address(0x1234), 5));
-        assertEq(token.getAccountCount(), 2);
-        accounts = token.getAccounts();
+        assertTrue(mixCreatorToken.transfer(address(0x1234), 5));
+        assertEq(mixCreatorToken.getAccountCount(), 2);
+        accounts = mixCreatorToken.getAccounts();
         assertEq(accounts.length, 2);
         assertEq(accounts[0], address(this));
         assertEq(accounts[1], address(0x1234));
-        (accounts, balances) = token.getAccountBalances();
+        (accounts, balances) = mixCreatorToken.getAccountBalances();
         assertEq(accounts.length, 2);
         assertEq(accounts[0], address(this));
         assertEq(accounts[1], address(0x1234));
@@ -139,13 +139,13 @@ contract MixCreatorTokenTest is DSTest {
         assertEq(balances[0], 5);
         assertEq(balances[1], 5);
 
-        assertTrue(token.transfer(address(0x1234), 1));
-        assertEq(token.getAccountCount(), 2);
-        accounts = token.getAccounts();
+        assertTrue(mixCreatorToken.transfer(address(0x1234), 1));
+        assertEq(mixCreatorToken.getAccountCount(), 2);
+        accounts = mixCreatorToken.getAccounts();
         assertEq(accounts.length, 2);
         assertEq(accounts[0], address(this));
         assertEq(accounts[1], address(0x1234));
-        (accounts, balances) = token.getAccountBalances();
+        (accounts, balances) = mixCreatorToken.getAccountBalances();
         assertEq(accounts.length, 2);
         assertEq(accounts[0], address(this));
         assertEq(accounts[1], address(0x1234));
@@ -153,14 +153,14 @@ contract MixCreatorTokenTest is DSTest {
         assertEq(balances[0], 4);
         assertEq(balances[1], 6);
 
-        assertTrue(token.transfer(address(0x2345), 4));
-        assertEq(token.getAccountCount(), 3);
-        accounts = token.getAccounts();
+        assertTrue(mixCreatorToken.transfer(address(0x2345), 4));
+        assertEq(mixCreatorToken.getAccountCount(), 3);
+        accounts = mixCreatorToken.getAccounts();
         assertEq(accounts.length, 3);
         assertEq(accounts[0], address(this));
         assertEq(accounts[1], address(0x1234));
         assertEq(accounts[2], address(0x2345));
-        (accounts, balances) = token.getAccountBalances();
+        (accounts, balances) = mixCreatorToken.getAccountBalances();
         assertEq(accounts.length, 3);
         assertEq(accounts[0], address(this));
         assertEq(accounts[1], address(0x1234));
@@ -172,12 +172,12 @@ contract MixCreatorTokenTest is DSTest {
     }
 
     function testSupportsInterface() public {
-        assertTrue(!token.supportsInterface(0x00000000));
-        assertTrue(!token.supportsInterface(0xffffffff));
-        assertTrue(token.supportsInterface(0x01ffc9a7));    // EIP165
+        assertTrue(!mixCreatorToken.supportsInterface(0x00000000));
+        assertTrue(!mixCreatorToken.supportsInterface(0xffffffff));
+        assertTrue(mixCreatorToken.supportsInterface(0x01ffc9a7));    // EIP165
 
         MixTokenInterfaceId mixTokenInterfaceId = new MixTokenInterfaceId();
-        assertTrue(token.supportsInterface(mixTokenInterfaceId.getInterfaceId()));
+        assertTrue(mixCreatorToken.supportsInterface(mixTokenInterfaceId.getInterfaceId()));
     }
 
 }
