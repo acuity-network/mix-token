d21f0171f0d87a67708920d96f45e22ad0e3e019
Jonathan Brown <jbrown@mix-blockchain.org>
2019-11-10 12:41:10 +0700

    pragma solidity ^0.5.12;

--- src/ERC165.sol
+++ src/ERC165.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.11;
+pragma solidity ^0.5.12;
 
 interface ERC165 {
     /// @notice Query if a contract implements an interface
--- src/MixTokenBurn.sol
+++ src/MixTokenBurn.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.11;
+pragma solidity ^0.5.12;
 
 import "mix-item-dag/MixItemDagOneParentOnlyOwner.sol";
 import "./MixTokenInterface.sol";
